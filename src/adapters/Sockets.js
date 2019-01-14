6c7e0e426331eba9c6bcc585413731c6544a14e9
George Pantazis <gcpantazis@gmail.com>
2019-01-14 11:32:17 -0600

    fix: apply prettier updates for all files

--- src/adapters/Sockets.js
+++ src/adapters/Sockets.js
@@ -1,19 +1,19 @@
 // @flow
-const SocketIO = require('socket.io-client');
+const SocketIO = require('socket.io-client')
 
-const CLIENT = typeof window !== 'undefined';
+const CLIENT = typeof window !== 'undefined'
 
 module.exports = class Sockets {
-	host: string;
+	host: string
 
 	constructor() {
-		this.host = 'https://metrics.sprucebot.com';
+		this.host = 'https://metrics.sprucebot.com'
 		if (CLIENT && window.METRICS_URL) {
-			this.host = window.METRICS_URL;
+			this.host = window.METRICS_URL
 		} else if (!CLIENT && process.env.METRICS_URL) {
-			this.host = process.env.METRICS_URL;
+			this.host = process.env.METRICS_URL
 		}
-		this.socket = new SocketIO();
+		this.socket = new SocketIO()
 	}
 	async sendMetrics(metrics) {}
-};
+}
--- src/sequelize/hooks.js
+++ src/sequelize/hooks.js
@@ -2,51 +2,51 @@
 module.exports = sequelize => {
 	sequelize.addHook('afterUpdate', (instance, hookOptions, cb) => {
 		try {
-			const modelName = instance.constructor.name;
+			const modelName = instance.constructor.name
 			log.metric({
 				type: 'sequelizeHook',
 				event: 'updated',
 				fields: hookOptions.fields,
 				modelName,
 				modelId: instance.id
-			});
+			})
 		} catch (e) {
-			console.log(e);
+			console.log(e)
 		}
 		if (typeof cb === 'function') {
-			return cb();
+			return cb()
 		}
-	});
+	})
 	sequelize.addHook('afterCreate', (instance, hookOptions, cb) => {
 		try {
-			const modelName = instance.constructor.name;
+			const modelName = instance.constructor.name
 			log.metric({
 				type: 'sequelizeHook',
 				event: 'created',
 				modelName,
 				modelId: instance.id
-			});
+			})
 		} catch (e) {
-			console.log(e);
+			console.log(e)
 		}
 		if (typeof cb === 'function') {
-			return cb();
+			return cb()
 		}
-	});
+	})
 	sequelize.addHook('afterDestroy', (instance, hookOptions, cb) => {
 		try {
-			const modelName = instance.constructor.name;
+			const modelName = instance.constructor.name
 			log.metric({
 				type: 'sequelizeHook',
 				event: 'destroyed',
 				modelName,
 				modelId: instance.id
-			});
+			})
 		} catch (e) {
-			console.log(e);
+			console.log(e)
 		}
 		if (typeof cb === 'function') {
-			return cb();
+			return cb()
 		}
-	});
-};
+	})
+}
